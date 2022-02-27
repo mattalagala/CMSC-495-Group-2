408fedd3606d3537839d1b1b30b011575ea9a65d
David Dickens <ddicknes@cdslogistics.net>
2022-02-27 13:01:53 -0500

    Add db queries
    still need more work on changing out some old queries and making sure they interact will with the front end.

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,23 @@
 // Update with your config settings.
 
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
-	development: {
-		client: "pg",
-		connection: {
-			database: "postgres",
-			user: "postgres",
-			password: "password",
-		},
-		pool: {
-			min: 2,
-			max: 10,
-		},
-		migrations: {
-			tableName: "knex_migrations",
-		},
-	},
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'CMSC_495',
+      user: 'postgres',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    },
+  }
+
 };
